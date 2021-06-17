897b6606d5b564c6e7d30cb40f4166af13f7d9db
RB <bobrovskiyroman@gmail.com>
2021-06-17 17:33:21 +0300

    menu

--- js/menu.js
+++ js/menu.js
@@ -1,24 +1,19 @@
-// import keyCodeList from "./keyCodeList";
+// import keyCodeList from "./keyCodeList.js";
 let btnRef = document.querySelector("#header__menuBtn");
 let menuRef = document.querySelector(".menu__content");
 let spanRef = document.querySelector("#header__closeMenuBtn");
+let contentRef = document.querySelector(".m__content");
 
-// btnRef.addEventListener("click", onOpenMenu);
+// let list = keyCodeList.reduce((acc, key) => [...acc, key], []);
+// console.log(list);
 
-// spanRef.addEventListener("click", () => {
-//   menuRef.style.display = "none";
-// });
-
-// spanRef.addEventListener("mouseover", (event) => {
-//   event.target.style.cursor = "pointer";
-// });
-
-btnRef.addEventListener("click", (event) => {
+btnRef.addEventListener("click", () => {
   menuRef.style.display = "block";
   btnRef.style.display = "none";
+  //   contentRef.textContent = list;
 });
 
-spanRef.addEventListener("click", (event) => {
+spanRef.addEventListener("click", () => {
   menuRef.style.display = "none";
   btnRef.style.display = "block";
 });

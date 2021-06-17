dd2493c98bb869842f16d223d3b9227a38d93341
RB <bobrovskiyroman@gmail.com>
2021-06-17 11:39:20 +0300

    pressAnyKey>display=none

---
+++ js/inputkey.js
@@ -0,0 +1,5 @@
+let inputKey = document.querySelector(".display__pressAnyKey");
+// let keyDisplay = document.querySelector(".display__keyDisplay");
+window.addEventListener("keydown", (event) => {
+  inputKey.style.display = "none";
+});

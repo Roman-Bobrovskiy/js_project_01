5d5b4cbc6cbcac976376b1e537b02f6726e57bfd
RB <bobrovskiyroman@gmail.com>
2021-06-17 22:35:35 +0300

    remove

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@
 let btnRef = document.querySelector("#header__menuBtn");
 let menuRef = document.querySelector(".menu__content");
 let spanRef = document.querySelector("#header__closeMenuBtn");
-let contentRef = document.querySelector(".m__content");
+// let contentRef = document.querySelector(".m__content");
 
 // let list = keyCodeList.reduce((acc, key) => [...acc, key], []);
 // console.log(list);
@@ -10,10 +10,20 @@ let contentRef = document.querySelector(".m__content");
 btnRef.addEventListener("click", () => {
   menuRef.style.display = "block";
   btnRef.style.display = "none";
+  btnRef.removeEventListener("click", btnRef);
   //   contentRef.textContent = list;
 });
 
 spanRef.addEventListener("click", () => {
   menuRef.style.display = "none";
   btnRef.style.display = "block";
+  spanRef.removeEventListener("click", spanRef);
+});
+
+window.addEventListener("keydown", (event) => {
+  if (event.code === "Escape") {
+    menuRef.style.display = "none";
+    btnRef.style.display = "block";
+  }
+  window.removeEventListener("keydown", window);
 });
